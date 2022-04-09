e3db8c7b71d0c401f48129acc41f5bee7b76ce4f
Yann Lima <71888064+Yann99999@users.noreply.github.com>
2022-04-08 22:44:09 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,16 @@
 var valor;
 var resultado;
-function botao(num) {
-   valor = document.calc.visor.value += num;
+
+function botao(num){
+    valor = document.calc.display.value += num;      
 }
 
-function reseta() {
-   document.calc.visor.value = "";
+
+function reseta(){
+    document.calc.display.value = "";
 }
 
-function calcula() {
+function calcula(){
    resultado = eval(valor);
-   document.calc.visor.value = resultado;
-   
+   document.calc.display.value = resultado;
 }
